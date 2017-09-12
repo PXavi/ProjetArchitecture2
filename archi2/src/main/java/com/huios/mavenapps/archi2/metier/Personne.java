eaa82e8f2893d6c63334002b4ef2ab3fb9df58f6
Xavier <xpersil@gmail.com>
2017-09-12 12:11:18 +0200

    maj PERSONNE

---
+++ archi2/src/main/java/com/huios/mavenapps/archi2/metier/Personne.java
@@ -0,0 +1,13 @@
+package com.huios.mavenapps.archi2.metier;
+
+/**
+ * Hello world!
+ *
+ */
+public class Personne 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
